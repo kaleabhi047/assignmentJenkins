ad59547009a6410b3cf47512d075242168fe3305
kaleabhi047 <abhi110kale@gmail.com>
2022-10-30 11:53:17 +0530

    done

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        int x=10;
+        int y=20;
+        int sum;
+        sum = x + y;
+        System.out.println("The Sum of two Numbers: " + sum);
+    }
+}
